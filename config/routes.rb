db94deae0316de5b807c1c87f08b3a476faef184
extern <extern.fx@gmail.com>
2011-08-27 18:23:01 +0300

    Some Updates For Devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SocialHub::Application.routes.draw do
 
   devise_for :users
 
-  root :to => "home#index"
+  root :to => "users#sign_in"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
