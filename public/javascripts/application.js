9ba0887802c7834b89c4518fddafb9f01a8063ad
Seva Rybakov <seva.rybakov@gmail.com>
2011-08-30 21:03:05 +0300

    Notifications now are able to disappear

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,6 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
+$(function(){
+	//binding close buttons to close the popups
+	$('.close').click( function(){
+		$(this).parent().fadeOut(2000);
+	});
+});
