c01a5ab9a8e50abd74cc0dbe87bd6209a3cf0d96
extern <extern.fx@gmail.com>
2011-09-01 17:52:34 +0300

    Update for Updating Wall

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
-  
+
   def subtitle(page_subtitle)
     content_for(:subtitle) { page_subtitle }
   end
-  
+
   def sidebar(page_sidebar)
     content_for(:sidebar) { page_sidebar }
   end
-  
+
   #defines content style based on presence of sidebar
   def content_class(sidebar_absent)
     if sidebar_absent
@@ -19,4 +19,6 @@ module ApplicationHelper
       "span12 columns"
     end
   end
+
 end
+
